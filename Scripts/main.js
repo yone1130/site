931d0742f2610274cedd6b25406b784a0073c170
よね/Yone <66835584+yone1130@users.noreply.github.com>
2023-03-02 19:56:10 +0900

    Update site

--- Scripts/main.js
+++ Scripts/main.js
@@ -6,8 +6,6 @@
 // No modification or reproduction of any kind is permitted.
 // 改変や複製を一切禁じます。
 // 
-let isActive_headerLinks
-
 document.addEventListener('DOMContentLoaded', () => {
     $('header').load('./elements/header.html');
     $('footer').load('./elements/footer.html');
