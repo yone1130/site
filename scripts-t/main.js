5660ba4b340b79e6adaf1dec97657d1773431aca
yone1130 <admin@yoneyo.com>
2023-07-05 15:37:19 +0900

    [temp] Changes directory name

---
+++ scripts-t/main.js
@@ -0,0 +1,13 @@
+// 
+// main.js | Yone Site
+// 
+// Copyright (c) 2022-2023 よね/Yone
+// 
+// No modification or reproduction of any kind is permitted.
+// 改変や複製を一切禁じます。
+// 
+
+$(() => {
+    $('header').load('./elements/header.html');
+    $('footer').load('./elements/footer.html');
+});
