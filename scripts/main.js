872ebc85ea1407c6ba5f5871dc9d09012cb832ae
yone1130 <admin@yoneyo.com>
2024-01-01 21:19:37 +0900

    Emergency Information

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ $(() => {
     // 2024-01-01 Tsunami Information
     $("main").prepend(`
         <a id="emergency" href="https://webapp.ydits.net/">
-            <p>YDITSからのお知らせ：津波警報、津波注意報が発表中されています（20時30分）</p>
+            <p>YDITSからのお知らせ：津波警報、津波注意報が発表されています（20時30分）</p>
         </a>
     `);
     $("#emergency").css({
